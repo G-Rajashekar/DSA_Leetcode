58d4282c3dd824fe760af039fd7746655d28aa65
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-06-03 12:09:31 +0530

    Time: 15 ms (31.40%) | Memory: 12.3 MB (26.12%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
+    int dp[1001][1001];
     int minSteps(int n) {
         if (n==1) return  0;
+        memset(dp,-1,sizeof(dp));
         return 1+helper(1,1,n);
     }
 
@@ -9,9 +11,10 @@ public:
         if (curA==n) return 0;
         if (curA>n) return 1000000;
 
+        if (dp[curA][copyA]!=-1) return dp[curA][copyA];
         int copyAllPaste=2+helper(curA+curA,curA,n);
         int paste=1+helper(curA+copyA,copyA,n);
 
-        return min(copyAllPaste,paste);
+        return dp[curA][copyA]=min(copyAllPaste,paste);
     }
 };
