801702311679609f25d37d30a355d6b2c9f165a9
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-06-08 10:53:35 +0530

    Time: 2 ms (52.82%) | Memory: 14.2 MB (44.11%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void solve(int num, int n, vector<int>& res) {
+        if (num > n)
+            return;
+        res.push_back(num);
+        for (int i = 0; i <= 9; i++) {
+            int newnum = num * 10 + i;
+            solve(newnum, n, res);
+        }
+    }
+    vector<int> lexicalOrder(int n) {
+        vector<int> res;
+        for (int i = 1; i <= 9; i++) {
+            solve(i, n, res);
+        }
+        return res;
+    }
+};
