8db4935c4c71c714efc66e0c0af382a62e00446a
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-06-27 13:02:07 +0530

    Time: 1513 ms (5.08%) | Memory: 612.7 MB (5.80%) - LeetSync

---
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestSubsequenceRepeatedK(string s, int k) {
+        int n=s.length();
+        string str="";
+        queue<string>q;
+        q.push(str);
+        string ans="";
+
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            for (char c='z';c>='a';c--){
+                string temp=it+c;
+                if (isExisit(s,temp,k)){
+                    if (ans.size()<temp.size() || temp>ans) ans=temp;
+                    q.push(temp);
+                }
+            }
+        }
+        return ans;
+    }
+
+    bool isExisit(string s,string temp,int k){
+        string repeatKTimes="";
+        while(k--){
+            repeatKTimes+=temp;
+        }
+        int j=0;
+
+        for (int i=0;i<s.length();i++){
+            if (s[i]==repeatKTimes[j]){
+                j++;
+            }
+            if (j==repeatKTimes.size()) return true;
+        }
+        return false;
+    }
+};
