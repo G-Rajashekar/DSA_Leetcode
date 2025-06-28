1c5f8e1f2f41b358e1839ca789674e786071d619
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-06-28 11:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (7.28%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
+    static bool comp(pair<int,int>&p1,pair<int,int>&p2){
+        return p1.first>p2.first;
+    }
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        int n=nums.size();
+        vector<pair<int,int>>Sorted;
 
-         for (int i=0;i<nums.size();i++){
-            int ele=nums[i];
-            pq.push({ele,i});
-            if (pq.size()>k) pq.pop();
-         }
+        for (int i=0;i<n;i++){
+            Sorted.push_back({nums[i],i});
+        }
 
-        set<int>st;
+        sort(Sorted.begin(),Sorted.end(),comp);
+        
+        vector<int>indexes;
 
-         while(!pq.empty()){
-            st.insert(pq.top().second);
-            pq.pop();
-         }
-
-         vector<int>ans;
-
-         for (int i=0;i<nums.size();i++){
-            if (st.count(i)) ans.push_back(nums[i]);
-         }
-         return ans;
+        for (int i=0;i<k;i++){
+            indexes.push_back(Sorted[i].second);
+        }
+        sort(indexes.begin(),indexes.end());
+        
+        for (int i=0;i<k;i++){
+            indexes[i]=nums[indexes[i]];
+        }
+        return indexes;
     }
 };
