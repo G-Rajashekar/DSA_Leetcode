ca7f6a59b7b1f4c4e7fe3e66b7bab01b26f30130
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-07-16 11:54:37 +0530

    Time: 8 ms (42.06%) | Memory: 96.1 MB (82.65%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -3,21 +3,20 @@ public:
     int maximumLength(vector<int>& nums) {
         int even=0;
         int odd=0;
-        
-        for (auto num:nums){
-            if (num%2) odd++;
-            else even++;
-        }
-
         int alternate=1;
         int parity=nums[0]%2;
 
-        for(int i=1;i<nums.size();i++){
-            int curParity=nums[i]%2;
-            if (curParity!=parity){
+        for(int i=0;i<nums.size();i++){
+           if (nums[i]%2) odd++;
+           else even++;
+
+           if (i>=1){
+                int curParity=nums[i]%2;
+                if (curParity!=parity){
                 alternate++;
                 parity=curParity;
-            }
+              }
+           }
         }
 
         return max({even,odd,alternate});
