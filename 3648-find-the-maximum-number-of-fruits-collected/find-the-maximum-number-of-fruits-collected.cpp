cd7bca357ed775c8c0978c098667ebfe4df91af8
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-08-07 18:59:15 +0530

    Time: 103 ms (48.70%) | Memory: 175.4 MB (12.99%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -2,42 +2,49 @@ class Solution {
 public:
     int maxCollectedFruits(vector<vector<int>>& fruits) {
         int n=fruits.size();
+        int ans=0;
+
         vector<vector<int>>dp1(n,vector<int>(n,-1));
         vector<vector<int>>dp2(n,vector<int>(n,-1));
-        int ans=0;
+
         for (int i=0;i<n;i++){
-            ans+=fruits[i][i];
-            fruits[i][i]=0;
+            for (int j=0;j<n;j++){
+                if (i==j){
+                    ans+=fruits[i][j];
+                }
+            }
         }
 
-        ans+=helper1(fruits,n-1,0,dp1,n);
-        ans+=helper2(fruits,0,n-1,dp2,n);
+        ans+=helper1(fruits,dp1,0,n-1,n);
+        ans+=helper2(fruits,dp2,n-1,0,n);
+
         return ans;
     }
 
-    int helper1(vector<vector<int>>& fruits,int i,int j, vector<vector<int>>&dp,int n){
-        if (i<0||j<0||i>=n||j>=n) return -1e9;
+    int helper1(vector<vector<int>>&fruits,vector<vector<int>>&dp,int i,int j,int n){
+        if (i>=n || j>=n || j<0) return 0;
+        if (i>=j) return 0;
         if (i==n-1 && j==n-1) return 0;
 
         if (dp[i][j]!=-1) return dp[i][j];
+        int op1=helper1(fruits,dp,i+1,j-1,n);
+        int op2=helper1(fruits,dp,i+1,j,n);
+        int op3=helper1(fruits,dp,i+1,j+1,n);
 
-        int op1=fruits[i][j]+helper1(fruits,i-1,j+1,dp,n);
-        int op2=fruits[i][j]+helper1(fruits,i,j+1,dp,n);
-        int op3=fruits[i][j]+helper1(fruits,i+1,j+1,dp,n);
-
-        return dp[i][j]=max(max(op1,op2),op3);
+        return dp[i][j]=fruits[i][j]+max({op1,op2,op3});
     }
 
-    int helper2(vector<vector<int>>& fruits,int i,int j, vector<vector<int>>&dp,int n){
-        if (i<0||j<0||i>=n||j>=n) return -1e9;
+    int helper2(vector<vector<int>>&fruits,vector<vector<int>>&dp,int i,int j,int n){
+        if (i>=n || j>=n || i<0) return 0;
+        if (i<=j) return 0;
         if (i==n-1 && j==n-1) return 0;
 
         if (dp[i][j]!=-1) return dp[i][j];
 
-        int op1=fruits[i][j]+helper2(fruits,i+1,j-1,dp,n);
-        int op2=fruits[i][j]+helper2(fruits,i+1,j,dp,n);
-        int op3=fruits[i][j]+helper2(fruits,i+1,j+1,dp,n);
+        int op1=helper2(fruits,dp,i-1,j+1,n);
+        int op2=helper2(fruits,dp,i,j+1,n);
+        int op3=helper2(fruits,dp,i+1,j+1,n);
 
-        return dp[i][j]=max(max(op1,op2),op3);
+        return dp[i][j]=fruits[i][j]+max({op1,op2,op3});
     }
 };
