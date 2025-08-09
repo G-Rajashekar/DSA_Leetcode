a6a397850c6d182806685a7b133f281e954e7d15
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-08-09 11:03:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (17.71%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) return false; 
-        int setBits=0;
-
-        for (int i=0;i<32;i++){
-            if (n & (1<<i)) setBits++;
-        }
-
-        return setBits==1?true:false;
+        return (n>0 && ((n&(n-1))==0));
     }
 };
