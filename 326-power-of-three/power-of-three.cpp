033d1c65aff36928cdb693fda04035654745dcf5
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-08-13 08:35:25 +0530

    Time: 4 ms (47.44%) | Memory: 8.9 MB (76.82%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -3,7 +3,10 @@ public:
     bool isPowerOfThree(int n) {
         if (n<=0) return false;
         for (int i=0;i<32;i++){
-            long long val=(long long)pow(3,i);
+            long long val=1;
+            for (int j=0;j<i;j++){
+                val*=3;
+            }
             if (val==n) return true;
         }
         return false;
