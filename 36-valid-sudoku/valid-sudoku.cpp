ab9b556452d5a27a7ad6db928de6305f4a461714
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-08-30 10:07:24 +0530

    Time: 7 ms (35.47%) | Memory: 25.8 MB (12.36%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -7,16 +7,19 @@ public:
 
         for (int i=0;i<9;i++){
             for (int j=0;j<9;j++){
-                if (board[i][j]=='.') continue;
+                char ch=board[i][j];
+                
+                if (ch=='.') continue;
 
                 int boxIndex=(i/3)*3+(j/3);
-                char ch=board[i][j];
+
                 if (rows[i].count(ch)||cols[j].count(ch)||boxes[boxIndex].count(ch)) return false;
                 rows[i].insert(ch);
                 cols[j].insert(ch);
                 boxes[boxIndex].insert(ch);
             }
         }
+
         return true;
     }
 };
