e16ae239dc3a194a5582cc944a483e84745c4ff0
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-10-08 17:23:39 +0530

    Time: 49 ms (25.46%) | Memory: 65 MB (64.94%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -1,29 +1,29 @@
-import java.util.*;
-
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        int n = spells.length;
-        int m = potions.length;
-        int[] ans = new int[n];
-        Arrays.sort(potions); // Sort potions once
+        Arrays.sort(potions);
+        int n=spells.length;
+        int m=potions.length;
+        int[] ans=new int[n];
 
-        for (int i = 0; i < n; i++) {
-            long minPotion = (success + spells[i] - 1) / spells[i]; // ceil(success / spell)
-            int idx = binarySearch(potions, minPotion);
-            ans[i] = m - idx; // remaining potions form successful pairs
+        for (int i=0;i<n;i++){
+              long target=(success+spells[i]-1)/spells[i];
+              int idx=helper(potions,target);
+              ans[i]=m-idx;
         }
-
         return ans;
+        
     }
 
-    private int binarySearch(int[] potions, long target) {
-        int l = 0, r = potions.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (potions[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+    int helper(int[] potions,long target){
+        int ans=0;
+        int l=0,r=potions.length-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if (potions[mid]>=target){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
             }
         }
         return l;
