89c66781f11fea7edda2277de4135da1cbc4be04
G-Rajashekar <123259170+G-Rajashekar@users.noreply.github.com>
2025-12-05 10:57:28 +0530

    Time: 1 ms (48.14%) | Memory: 43.5 MB (31.42%) - LeetSync

---
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countPartitions(int[] nums) {
+        int n=nums.length;
+        int[] prefix=new int[n];
+        int[] sufix=new int[n];
+
+        prefix[0]=nums[0];
+
+        for (int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+
+        sufix[n-1]=0;
+
+        for (int i=n-2;i>=0;i--){
+            sufix[i]=sufix[i+1]+nums[i+1];
+        }
+
+        int ans=0;
+
+        for (int i=0;i<n;i++){
+            if ((prefix[i]+sufix[i])%2==0) ans++;
+        }
+        return ans==0?0:ans-1;
+    }
+}
